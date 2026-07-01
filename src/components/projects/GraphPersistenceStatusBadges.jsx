14ebea6bee4a35b8a779de77c8f4b90440d843ea
xcz <op.ondrejpokorny@gmail.com>
2026-07-01 21:03:35 +0200

    Use graph persistence entity constants

--- src/components/projects/GraphPersistenceStatusBadges.jsx
+++ src/components/projects/GraphPersistenceStatusBadges.jsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import OptionalPersistenceStatusPanel from '@/components/projects/OptionalPersistenceStatusPanel';
+import { GRAPH_PERSISTENCE_ENTITIES } from '@/lib/optionalEntityGroups';
 
 export default function GraphPersistenceStatusBadges({ className = '' }) {
-  return <OptionalPersistenceStatusPanel entityNames={["CodeRelation", "CodeSymbol"]} className={className} />;
+  return <OptionalPersistenceStatusPanel entityNames={GRAPH_PERSISTENCE_ENTITIES} className={className} />;
 }
