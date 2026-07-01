0c79854b3e36b624478d32716b468ca4b481d0a6
xcz <op.ondrejpokorny@gmail.com>
2026-07-01 21:00:25 +0200

    Use optional persistence status panel in graph badges

--- src/components/projects/GraphPersistenceStatusBadges.jsx
+++ src/components/projects/GraphPersistenceStatusBadges.jsx
@@ -1,12 +1,6 @@
 // @ts-nocheck
-import CodeRelationStatusBadge from '@/components/projects/CodeRelationStatusBadge';
-import CodeSymbolStatusBadge from '@/components/projects/CodeSymbolStatusBadge';
+import OptionalPersistenceStatusPanel from '@/components/projects/OptionalPersistenceStatusPanel';
 
 export default function GraphPersistenceStatusBadges({ className = '' }) {
-  return (
-    <div className={`flex flex-wrap gap-1.5 ${className}`}>
-      <CodeRelationStatusBadge />
-      <CodeSymbolStatusBadge />
-    </div>
-  );
+  return <OptionalPersistenceStatusPanel entityNames={["CodeRelation", "CodeSymbol"]} className={className} />;
 }
