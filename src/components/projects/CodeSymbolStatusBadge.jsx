ad501d2f3c6d30c46b95f17e0d9af5ae72329785
xcz <op.ondrejpokorny@gmail.com>
2026-07-01 20:22:09 +0200

    Add CodeSymbol status badge

---
+++ src/components/projects/CodeSymbolStatusBadge.jsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import OptionalEntityStatusBadge from '@/components/projects/OptionalEntityStatusBadge';
+
+export default function CodeSymbolStatusBadge({ className = '' }) {
+  return <OptionalEntityStatusBadge entityName="CodeSymbol" className={className} />;
+}
